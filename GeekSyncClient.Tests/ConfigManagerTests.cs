3324bc718aea172c9f4e87c1e76969b1641eb591
Jarek Kruza <jarek@kruza.pl>
2020-05-16 18:35:29 +0200

    AB#152 AB#151 pairing

--- GeekSyncClient.Tests/ConfigManagerTests.cs
+++ GeekSyncClient.Tests/ConfigManagerTests.cs
@@ -3,8 +3,10 @@ using Xunit;
 using System;
 using System.Threading;
 using GeekSyncClient;
+using GeekSyncClient.Config;
 using System.Xml.Linq;
 using System.IO;
+using System.Linq;
 
 namespace GeekSyncClient.UnitTests
 {
@@ -53,7 +55,46 @@ namespace GeekSyncClient.UnitTests
 
         }
 
+        [Fact]
+        public void PairingTest()
+        {
+            string peer1conf=".peer.1.conf";
+            string peer2conf=".peer.2.conf";
 
+            FileInfo f1=new FileInfo(peer1conf);
+            if (f1.Exists) f1.Delete();
+
+            FileInfo f2=new FileInfo(peer2conf);
+            if (f2.Exists) f2.Delete();
+            
+            ConfigManager peer1=new ConfigManager(peer1conf);
+            ConfigManager peer2=new ConfigManager(peer2conf);
+
+
+            Assert.NotEqual(peer1.Config.MyID,peer2.Config.MyID);
+            Assert.Single(peer1.Config.Peers);
+            Assert.Single(peer2.Config.Peers);
+
+            Guid channelID=peer1.PeerWith(peer2);
+
+            Assert.Single(peer1.Config.Peers.Where(x=>x.ChannelID==channelID));
+            Assert.Single(peer2.Config.Peers.Where(x=>x.ChannelID==channelID));
+
+            ConfigManager newPeer1=new ConfigManager(peer1conf);
+            ConfigManager newPeer2=new ConfigManager(peer2conf);
+
+            Assert.Single(newPeer1.Config.Peers.Where(x=>x.ChannelID==channelID));
+            Assert.Single(newPeer2.Config.Peers.Where(x=>x.ChannelID==channelID));
+
+            Peer p1=newPeer1.Config.Peers.Single(x=>x.ChannelID==channelID);
+            Peer p2=newPeer2.Config.Peers.Single(x=>x.ChannelID==channelID);
+
+            Assert.Equal(newPeer1.Config.MyID,p2.PeerID);
+            Assert.Equal(newPeer1.Config.MyPublicKey,p2.PeerPublicKey);
+
+            Assert.Equal(newPeer2.Config.MyID,p1.PeerID);
+            Assert.Equal(newPeer2.Config.MyPublicKey,p1.PeerPublicKey);
+        }
 
 
 
--- GeekSyncClient/ConfigManager.cs
+++ GeekSyncClient/ConfigManager.cs
@@ -12,47 +12,83 @@ namespace GeekSyncClient
     {
 
         public ClientConfig Config;
+        private string configFileName=".geeksync-client.conf";
 
-        public ConfigManager(string configFile)
+        public void ReadConfig()
         {
-            FileInfo config = new FileInfo(configFile);
-            if (!config.Exists)
-            {
-                //Console.WriteLine("Config file does not exist, creating default.");
+            FileInfo config = new FileInfo(configFileName);
+            StreamReader sr = config.OpenText();
+            string configtext = sr.ReadToEnd();
+            sr.Close();
+            Config = JsonSerializer.Deserialize<ClientConfig>(configtext);
+        }
 
+        public void WriteConfig()
+        {
+            FileInfo config = new FileInfo(configFileName);
+            config.Delete();
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(Config);
+            StreamWriter sw = config.AppendText();
+            sw.WriteLine(jsonString);
+            sw.Close();
+        }
+
+        public void LoadOrCreateConfig()
+        {
+            FileInfo config = new FileInfo(configFileName);
+            if (config.Exists)
+            {
+                ReadConfig();
+            }
+            else
+            {
                 Config = new ClientConfig();
 
                 RSAHelper tempRSA = new RSAHelper();
 
                 Config.RSAkeys = tempRSA.KeysXml;
                 Config.MyID = Guid.NewGuid();
-                Config.MyPublicKey=tempRSA.MyPublicKey;
+                Config.MyPublicKey = tempRSA.MyPublicKey;
 
                 Peer self = new Peer() { PeerID = Config.MyID, ChannelID = Guid.NewGuid(), PeerPublicKey = tempRSA.MyPublicKey };
 
                 Config.Peers = new List<Peer>();
                 Config.Peers.Add(self);
-
-
-
-                string jsonString;
-                jsonString = JsonSerializer.Serialize(Config);
-                // Console.WriteLine("-----");
-                // Console.WriteLine(jsonString);
-                // Console.WriteLine("-----");
-                StreamWriter sw = config.AppendText();
-                sw.WriteLine(jsonString);
-                sw.Close();
+                WriteConfig();
             }
-
-            StreamReader sr = config.OpenText();
-            string configtext = sr.ReadToEnd();
-            sr.Close();
-            Config = JsonSerializer.Deserialize<ClientConfig>(configtext);
-
         }
 
+        public ConfigManager()
+        {
+          LoadOrCreateConfig();
+          
+        }
+        public ConfigManager(string configFile)
+        {
+            configFileName=configFile;
+            LoadOrCreateConfig();
+        }
 
+        public void AddPeer(Guid channelID, Guid peerID, string peerPublicKey)
+        {
+            Peer peer = new Peer() { ChannelID = channelID, PeerID = peerID, PeerPublicKey = peerPublicKey };
+            AddPeer(peer);
+        }
+
+        public void AddPeer(Peer peer)
+        {
+            Config.Peers.Add(peer);
+            WriteConfig();
+        }
+
+        public Guid PeerWith(ConfigManager other)
+        {
+            Guid channelID=Guid.NewGuid();
+            this.AddPeer(channelID,other.Config.MyID,other.Config.MyPublicKey);
+            other.AddPeer(channelID,this.Config.MyID, this.Config.MyPublicKey);
+            return channelID;
+        }
 
     }
 }
